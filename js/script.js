4693b7009bbf91972aeb8551d79967ff04be5cf5
Egi Gustian Maulana <119639520+EgiGustian@users.noreply.github.com>
2025-11-13 13:43:30 +0700

    menambahkan fitur UI search dan memperbaiki bug

--- js/script.js
+++ js/script.js
@@ -1,16 +1,30 @@
-// toggle class active
+// toggle class active menu
 const navbarNav = document.querySelector('.navbar-nav');
-
 // ketika menu di klik
-document.querySelector('#menu').onclick = () => {
+document.querySelector('#menu').onclick = (e) => {
   navbarNav.classList.toggle('active');
+  e.preventDefault();
 }
 
-// Klik di luar sidebar untuk menghilangkan nav
+// toggle class active search form
+const searchForm = document.querySelector('.search-form');
+const searchBox = document.querySelector('#search-box');
+// ketika search di klik
+document.querySelector('#search-btn').onclick = (e) => {
+  searchForm.classList.toggle('active');
+  searchBox.focus();
+  e.preventDefault();
+}
+
+// Klik di luar elemet
 const menuNav = document.querySelector('#menu');
+const searchBtn = document.querySelector('#search-btn');
 
 document.addEventListener('click', function(e) {
   if(!menuNav.contains(e.target) && !navbarNav.contains(e.target)) {
     navbarNav.classList.remove('active');
+  }
+  if(!searchBtn.contains(e.target) && !searchForm.contains(e.target)) {
+    searchForm.classList.remove('active');
   }
 })
